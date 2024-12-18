4f4dee1908d4e4463072728318277ef7102d4467
Glitch (glitch-hello-node) <none>
2024-12-18 03:09:58 +0000

    🍫🚁 Checkpoint ./index.js:71772514/14

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ app.post("/getchat", async (req, res) => {
   const roomId = req.body.webhook_event.room_id;
   const messageId = req.body.webhook_event.message_id;
   const sendername = await getSenderName(accountId, roomId);
-  const welcomeId = req.body.webhook_event.body.replace(/\D{0,100}/,'')
+  const welcomeId = req.body.webhook_event.body.replace(/\D/g,'')
 
   if ((body.match(/\)/g) || []).length >= 20) {
     await blockMembers(body, message, messageId, roomId, accountId, sendername);
