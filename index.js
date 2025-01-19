9d87f7b22254c72af9e6bcf9c32202b0218c45b5
Glitch (glitch-hello-node) <none>
2025-01-19 12:47:20 +0000

    🏁🐈 Checkpoint ./index.js:71772514/138

--- index.js
+++ index.js
@@ -503,7 +503,9 @@ async function janken(body, message, messageId, roomId, fromaccountId) {
 }
 async function saikoro(body, message, messageId, roomId, fromaccountId) {
   const saikoro = [...body.matchAll(/\d+(?=d)/g)].map((saikoro) => saikoro[0]);
-
+  for(let s = 0; s > saikoro; s++){
+    console.log(s)
+  }
   const men = [...body.matchAll(/(?<=d)\d+/g)].map((men) => men[0]);
   const number = Math.floor(Math.random() * men) + 1;
   if (men > 0) {
