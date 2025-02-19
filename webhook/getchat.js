94c0424e718393051819f9bc97ff4f25ed0d88c5
Glitch (nyankonoheya) <none>
2025-02-19 09:05:56 +0000

    🏪🍓 Checkpoint ./webhook/getchat.js:71772514/758 ./middleware/sign.js:71772514/678 ./webhook/mention.js:71772514/780

---
+++ webhook/getchat.js
@@ -0,0 +1,32 @@
+const reqcheck = require('../middleware/sign');
+
+const emoji = require('../module/emoji');
+const mention = require('../module/mention');
+const zalgo = require('../module/zalgo');
+const pp = require('../module/pp');
+
+async function getchat(req, res) {
+  const c = await reqcheck(req);
+  if (c !== "ok") {
+    return res.sendStatus(400);
+  }
+
+  console.log(req.body);
+  const { body, account_id: accountId, room_id: roomId } = req.body.webhook_event;
+
+  if (accountId === 9587322) {
+    return res.sendStatus(200);
+  }
+
+  const handlers = [emoji, mention, zalgo, pp];
+
+  for (const handler of handlers) {
+    if (await handler(body, roomId, accountId) === "ok") {
+      return res.sendStatus(200);
+    }
+  }
+
+  res.sendStatus(200);
+}
+
+module.exports = getchat;
---
+++ webhook/mention.js
@@ -0,0 +1,17 @@
+const msedit = require('../ctr/message');
+
+async function mentionWebhook(req, res) {
+    const accountId = req.body.webhook_event.from_account_id;
+    const roomId = req.body.webhook_event.room_id;
+    const messageId = req.body.webhook_event.message_id;
+    const body = req.body.webhook_event.body;  
+  await msedit.readmessage(body, messageId, roomId, accountId);
+    if (body.includes("削除")) {
+        await msedit.deleteMessages(body, messageId, roomId, accountId);
+        return res.sendStatus(200);
+    }
+    
+    res.sendStatus(500);
+}
+
+module.exports = mentionWebhook;
