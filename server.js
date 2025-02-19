f4ef544c4056371524d3f45c1bf0f40fb1d303a2
Glitch (nyankonoheya) <none>
2025-02-19 08:54:01 +0000

    🌗✋ Checkpoint ./bot.js:71772514/132 ./保存:71772514/65 ./server.js:71772514/1480

---
+++ server.js
@@ -0,0 +1,29 @@
+"use strict";
+const express = require("express");
+const app = express();
+  app.listen(3000, () => {
+    console.log(`${process.pid} started`);
+  });
+
+const https = require('https');
+const mention = require('./webhook/mention');
+const getchat = require('./webhook/getchat');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.end(JSON.stringify(process.versions, null, 2));
+});
+
+app.post("/mention", (req, res) => {
+  mention(req, res);
+});
+
+app.post("/getchat", (req, res) => {
+  getchat(req, res);
+});
+
+app.post("/test", (req, res) => {
+  getchat(req, res);
+});
