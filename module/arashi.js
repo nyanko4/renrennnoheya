de2ab9f49ada7428c813a921af06566fa9667212
Glitch (nyankonoheya) <none>
2025-02-19 10:04:11 +0000

    ⚽️🍬 Checkpoint ./module/arashi.js:71772514/1391

--- module/arashi.js
+++ module/arashi.js
@@ -9,7 +9,7 @@ const m = [
   "(*)", "(h)", "(F)", "(cracker)", "(eat)", "(^)", "(coffee)", "(beer)", 
   "(handshake)", "(y)", /\[p\D+\d+\]/, ")", /\[toall\]/, /\[to:\d+\]/
 ];
-//絵文字に対して反応します
+//荒らしに対して反応します
 async function arashi(body, roomId, accountId) {
     let count = 0;
     const bodyChars = [...body];
@@ -19,7 +19,7 @@ async function arashi(body, roomId, accountId) {
             count++;
         }
     });
-    console.log("emoji", count)
+    console.log("arashi", count)
     if (count >= 30) {
         block.blockMember(roomId, accountId);
         return "ok";
@@ -27,6 +27,25 @@ async function arashi(body, roomId, accountId) {
 
   return;
 }
+const zzalgo = /[\u0300-\u036F\u1AB0-\u1AFF\u1DC0-\u1DFF\u20D0-\u20FF\uFE20-\uFE2F]/;
+
+async function zalgo(body, roomId, accountId) {
+  let zalgoCount = 0;
+
+  for (let char of body) {
+    if (zzalgo.test(char)) {
+      zalgoCount++;
+    }
+  }
+  console.log("zalgo", zalgoCount)
+  if (zalgoCount >= 500) {
+    await block.blockMember(roomId, accountId);
+    return "ok";
+  }
+  
+  return;
+};
 module.exports = {
-  arashi
+  arashi,
+  zalgo
 };
