bd71c2371abf709b85eee89125959266b136a138
Glitch (nyankonoheya) <none>
2025-02-20 03:09:18 +0000

    👷🙉 Checkpoint ./module/command.js:71772514/1836 ./保存:71772514/53 ./webhook/getchat.js:71772514/18

--- module/command.js
+++ module/command.js
@@ -1,2 +1,20 @@
-async func
-module.exports　= {}
+const sendchatwork = require("../ctr/message").sendchatwork;
+const commands = {};
+async function test(body, messageId, roomId, accountId) {
+  const message = body.replace(/\/.*?\/|\s+/g, "");
+  const command = getCommand(body);
+  if (command && commands[command]) {
+    //await commands[command](body, message, messageId, roomId, accountId);
+  } else if (command){
+    sendchatwork(
+      "現在bot停止中のため要件のある場合は本垢に言ってください",
+      roomId
+    );
+  }
+}
+function getCommand(body) {
+  const pattern = /\/(.*?)\//;
+  const match = body.match(pattern);
+  return match ? match[1] : null;
+}
+module.exports = { test };
