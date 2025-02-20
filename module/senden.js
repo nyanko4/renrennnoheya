fd7687b57ebb4b4cf1494300f1f0b3d71964b17c
Glitch (nyankonoheya) <none>
2025-02-20 07:09:08 +0000

    ⛰☎️ Checkpoint ./module/senden.js:71772514/3570 ./bot.js:71772514/1778

--- module/senden.js
+++ module/senden.js
@@ -1,3 +1,38 @@
 const block = require("../ctr/filter");
 const isAdmin = require("../ctr/cwdata").isUserAdmin;
 const sendchatwork = require("../ctr/message").sendchatwork;
+async function senden(body, messageId, roomId, accountId) {
+  try {
+        if (body.match(/\www.chatwork.com\/g/g)) {
+      await sendenkinshi(body, messageId, roomId, accountId);
+    }
+    if (body.match(/\www.chatwork.com\/#!join/g)) {
+      await sendenkinshi(body, messageId, roomId, accountId);
+    }
+    if (body.match(/\https:\/\/odaibako.net\/u/g)) {
+      await sendenkinshi(
+        body,
+        message,
+        messageId,
+        roomId,
+        accountId,
+        sendername
+      );
+    }
+    if (body.match(/\https:\/\/scratch.mit.edu/g)) {
+      await sendenkinshi(
+        body,
+        message,
+        messageId,
+        roomId,
+        accountId,
+        sendername
+      );
+    }
+    if (body.match(/\https:\/\/padlet.com/g)) {
+      await sendenkinshi(body,messageId,roomId,accountId)
+    }
+  } catch (error) {
+    console.error("エラー※宣伝", error)
+  }
+}
