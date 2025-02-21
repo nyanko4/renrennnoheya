89cf47ad47bc5cb99fbc2a48c82d06f5ac460aef
Glitch (nyankonoheya) <none>
2025-02-21 02:58:15 +0000

    ⛸😺 Checkpoint ./commands/ousama.js:71772514/1693 ./ousamagame/ousama.txt:71772514/8

--- commands/ousama.js
+++ commands/ousama.js
@@ -7,12 +7,23 @@ async function ousamagame(body, message, messageId, roomId, accountId) {
     if (!members || members.length === 0) {
       return;
     }
-    fs.appendFile('/ousamagame/ousama.txt', 'サンプルの文字列', function(err) {})
-  } catch (error) {
+    const randomIndex = Math.floor(Math.random() * members.length);
+    const randomMember = members[randomIndex];
+  }
+    fs.writeFile('./ousamagame/ousama.txt', r, function(err) {
+      console.log(err)
+    })
+} catch (error) {
     await sendchatwork(
       `[rp aid=${accountId} to=${roomId}-${messageId}][pname:${accountId}]さん\nエラー`,
       roomId
     );
   }
+}
+async function ousamakekka(body, message, messageId, roomId, accountId) {
+fs.readFile('./ousamagame/ousama.txt', 'utf8', function(err, data) {   
+  console.log(data);
+      console.log(err)
+  })
 }
 module.exports = ousamagame
