c375b7b8b1474619a52439a272a33ce4d0cfea65
nyanko4 <nyankobetsuaka@outlook.com>
2025-07-02 13:02:17 +0900

    Update sign.js

--- middleware/sign.js
+++ middleware/sign.js
@@ -2,7 +2,6 @@ const crypto = require('crypto');
 
 const webhookTokens = [
     Buffer.from(process.env.webhookToken, 'base64'),
-  　Buffer.from(process.env.t, 'base64'),
 ];
 
 async function trustSignature(req) {
