437a9505d7af91228277b91c68977a01a6de77d3
nyanko4 <nyankobetsuaka@outlook.com>
2025-07-02 17:01:28 +0900

    Update message.js

--- ctr/message.js
+++ ctr/message.js
@@ -52,11 +52,12 @@ async function deleteMessages(body, messageId, roomId, accountId) {
 }
 
 //メッセージに既読をつける
-async function readmessage(roomId) {
+async function readmessage(roomId, messageId) {
   try {
     await axios.put(
       `https://api.chatwork.com/v2/rooms/${roomId}/messages/read`,
       {
+        new URLSearchParams({ message_id: messageId }),
         headers: {
           "X-ChatWorkToken": CHATWORK_API_TOKEN,
           "Content-Type": "application/x-www-form-urlencoded",
