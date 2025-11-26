70a0c4bdd4757f2edbe631aa57802234881417a1
nyanko4 <nyankobetsuaka@outlook.com>
2025-11-26 22:34:18 +0900

    Create Task.js

---
+++ webhook/cron/Task.js
@@ -0,0 +1,38 @@
+const { CronJob } = require("cron");
+const supabase = require("../supabase/client");
+const { commentRankingMinute } = require("../module/commentRanking");
+const { getMessages } = require("../ctr/message");
+
+function startTask() {
+  new CronJob(
+    "5 * * * * *",
+    async () => {
+      commentRankingMinute()
+    },
+    null,
+    true,
+    "Asia/Tokyo"
+  );
+}
+
+function startDailyTask() {
+  new CronJob(
+    "0 0 0 * * *",
+    async () => {
+      await supabase
+        .from("comment_num")
+        .delete()
+        .neq("accountId", 0);
+    await getMessages(374987857);
+    },
+    null,
+    true,
+    "Asia/Tokyo"
+  );
+}
+
+
+module.exports = {
+  startTask,
+  startDailyTask,
+};
