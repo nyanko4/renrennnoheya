315cb23eb6bb3ffe5d105484232ce94e373cba39
nyanko4 <nyankobetsuaka@outlook.com>
2025-11-26 22:36:38 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,7 @@
 "use strict";
-const cluster = require("cluster");
-const os = require("os");
-const numCPUs = os.cpus().length;
 
-if (cluster.isMaster) {
-  for (let i = 0; i < numCPUs; i++) {
-    cluster.fork();
-  }
-  cluster.on("exit", (worker) => {
-    console.log(`Worker ${worker.process.pid} died. Restarting...`);
-    cluster.fork();
-  });
-} else {
-  require("./server");
-}
+const { startTask, startDailyTask } = require("../cron/dailyTask");
+startTask();
+startDailyTask();
+
+require("./server");
