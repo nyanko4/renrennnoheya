f2c8c94ea50a98c025c879203c2aeb09c61d39e1
nyanko4 <nyankobetsuaka@outlook.com>
2025-11-27 21:35:51 +0900

    Update commentRanking.js

--- module/commentRanking.js
+++ module/commentRanking.js
@@ -3,22 +3,45 @@ const { getMessages } = require("../ctr/message");
 
 async function commentRanking(body, messageId, roomId, accountId) {
   const data = await getRankingCommentNum(accountId);
-  const messageNum = data.number + 1;
-  await supabase
-    .from("message_num")
-    .upsert([
-      {
-        account_id: accountId,
-        number: messageNum,
-        },
-      ]);
+  const messageNum = (data?.number ?? 0) + 1;
+
+  await supabase.from("message_num").upsert([
+    {
+      account_id: accountId,
+      number: messageNum,
+    },
+  ]);
 }
 
 async function commentRankingMinute(roomId) {
-  const messages = getMessages(roomId);
-  
-  
-  const messageNumSupabase = await getRankingCommentNum(accountId);
+  let accountMessageNum = {};
+  const messages = await getMessages(roomId);
+
+  for (const message of messages) {
+    const accountId = message.account.account_id;
+
+    if (!accountMessageNum[accountId]) {
+      accountMessageNum[accountId] = 0;
+    }
+
+    accountMessageNum[accountId] += 1;
+  }
+
+  for (const accountId of Object.keys(accountMessageNum)) {
+    const minuteCount = accountMessageNum[accountId];
+
+    const dbData = await getRankingCommentNum(accountId);
+    const dbCount = dbData?.number ?? 0;
+
+    const correctCount = Math.max(minuteCount, dbCount);
+
+    await supabase.from("message_num").upsert([
+      {
+        account_id: accountId,
+        number: correctCount,
+      },
+    ]);
+  }
 }
 
 async function getRankingCommentNum(accountId) {
@@ -26,13 +49,9 @@ async function getRankingCommentNum(accountId) {
     .from("message_num")
     .select("number")
     .eq("account_id", accountId)
-    .single()
+    .single();
 
-  if (error) {
-    console.error(error);
-    return null;
-  }
-  
+  if (error) return null;
   return data;
 }
 
