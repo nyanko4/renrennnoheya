811f87a0f69a61dd4b1728ee0568920e675d9f7d
nyanko4 <nyankobetsuaka@outlook.com>
2025-11-29 00:51:05 +0900

    Update Task.js

--- cron/Task.js
+++ cron/Task.js
@@ -23,7 +23,7 @@ function startTask() {
 
 function startDailyTask() {
   new CronJob(
-    "0 35 0 * * *",//59 23
+    "0 55 0 * * *",
     async () => {
       await dailyCommentRanking(kotya);
     },
